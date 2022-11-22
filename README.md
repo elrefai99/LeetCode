1a40223ba53e410e55446663f35da9806c4f9318
mohamed-elrefai <mohamed.mostafa0699@gmail.com>
2022-11-22 08:12:05 +0200

    chore: 205

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 ## Will write the code by javascript and typescript✌.
 
-- Easy: 3 / 607
+- Easy: 5 / 607
 - Medium: 1 / 1322
 - Hard: 1 / 549
---
+++ code/easy/205. Isomorphic Strings/205. Isomorphic Strings.js
@@ -0,0 +1,20 @@
+var isIsomorphic = function(s, t) {
+    var mapS = {};
+    var mapT = {};
+
+    for(var i in s){
+        var valueS = s[i];
+        var valueT = t[i];
+        if(!mapS[valueS]){
+            mapS[valueS] = valueT;
+        } else if(mapS[valueS] != valueT) { 
+            return false;
+        }
+        if(!mapT[valueT]){
+            mapT[valueT] = valueS;
+        } else if(mapT[valueT] != valueS) { 
+            return false;
+        }
+    }
+    return true;
+};
