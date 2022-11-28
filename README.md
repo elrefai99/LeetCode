59fd36a2081df6da6c6b9e309249c1dd9dc3e162
mohamed-elrefai <mohamed.mostafa0699@gmail.com>
2022-11-28 17:52:15 +0200

    chore: 235. Lowest Common Ancestor of a BST

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 ## Will write the code by javascript and typescript✌.
 
 - Easy: 13 / 607
-- Medium: 5 / 1322
+- Medium: 6 / 1322
 - Hard: 3 / 549
---
+++ code/medium/235. Lowest Common Ancestor of a Binary Search Tree/235. Lowest Common Ancestor of a Binary Search Tree.js
@@ -0,0 +1,13 @@
+var lowestCommonAncestor = function(root, p, q) {
+    while(true){
+        var value =  root.val;
+
+        if(p.val >= value && value >= q.val || p.val <= value && value <= q.val){
+            return root;
+        } else if(p.val > value && q.val > value){
+            root =  root.right;
+        } else {
+            root =  root.left;
+        }
+    }
+};
